1915ee0877b8928f29fbc73e18305d1e89e3846d
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-04-26 22:24:21 +0300

    feat: add methods for login-card validators

--- src/app/auth/components/login-card/login-card.component.ts
+++ src/app/auth/components/login-card/login-card.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-card',
   templateUrl: './login-card.component.html',
-  styleUrls: ['./login-card.component.scss']
+  styleUrls: ['./login-card.component.scss'],
 })
 export class LoginCardComponent implements OnInit {
+  form!:FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      pass: new FormControl('', [Validators.required, this.checkForLength, this.checkUpperLower, this.checkMixture, this.checkSpecialChar]),
+    });
   }
 
+  checkForLength(control: FormControl) {
+    if (control.value.length === 0) { return; }
+
+    if (control.value.length < 8) {
+      return {
+        lengthError: true,
+      };
+    }
+  }
+
+  checkUpperLower(control: FormControl) {
+    const rexP:RegExp = /(?=.*[a-z])(?=.*[A-Z])/;
+    if (control.value.length === 0) { return; }
+
+    if (!control.value.match(rexP)) {
+      return {
+        upperLowerError: true,
+      };
+    }
+  }
+
+  checkMixture(control: FormControl) {
+    const rexP:RegExp = /[A-Z][a-z]+/;
+    const rexP2:RegExp = /[0-9]+/;
+    if (control.value.length === 0) { return; }
+    if (!control.value.match(rexP) && !control.value.match(rexP2)) {
+      return {
+        mixtureError: true,
+      };
+    }
+  }
+
+  checkSpecialChar(control: FormControl) {
+    const rexP:RegExp = /[!@#$&*%]+/;
+    if (control.value.length === 0) { return; }
+    if (!control.value.match(rexP)) {
+      return {
+        specialCharError: true,
+      };
+    }
+  }
 }
