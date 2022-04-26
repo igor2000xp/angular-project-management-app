d6c5a1a7066a7e03c47da7124f26b972eb0cff4c
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-04-26 23:02:36 +0300

    feat: add LoginService with RestApi

---
+++ src/app/auth/services/loginService.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+
+const BASE = 'http://localhost:4000';
+const SIGNUP = `${BASE}/signup`;
+const SIGNIN = `${BASE}/signin`;
+const USERS = `${BASE}/users`;
+
+@Injectable()
+export class ApiService {
+  constructor(private httpClient: HttpClient) { }
+
+  //signup: (user: {name, login, password}) => {id, name, login}, signin: (user: {login, password}) => {token}
+  public authenticate(user: User, mode: string): Observable<User> {
+    const url = mode === 'signup' ? SIGNUP : SIGNIN;
+    const headers = new HttpHeaders()
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json');
+    const body = JSON.stringify(user);
+    return this.httpClient.post<User>(url, body, { headers: headers });
+  }
+
+  public getUsers(token: string): Observable<User[]> {
+    const headers = new HttpHeaders().set('accept', 'application/json').set('Authorization', `Bearer ${token}`);
+    return this.httpClient.get<User[]>(USERS, { headers: headers });
+  }
+}
