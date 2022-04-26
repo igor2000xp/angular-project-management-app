2d77af3a0f909fce1c44975ec7e53bc2c19680c9
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-04-26 22:24:49 +0300

    feat: update imports && exports auth.module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginCardComponent } from './components/login-card/login-card.component';
 
 @NgModule({
   declarations: [
     LoginCardComponent,
   ],
+  exports: [
+    LoginCardComponent,
+  ],
   imports: [
+    BrowserModule,
     CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
 })
 export class AuthModule { }
