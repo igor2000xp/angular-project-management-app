c4029f93e353c0b84bb5212e952c246ff9d92c1b
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-04-29 00:40:07 +0300

    fix: delete auth-routing component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,17 +3,14 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegistrationCardComponent } from './components/login-card/registration-card.component';
-import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [RegistrationCardComponent],
   imports: [
-    AuthRoutingModule,
     BrowserModule,
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
-    AuthRoutingModule,
   ],
 })
 export class AuthModule { }
