42a944c49506c9e218cf55346d4e73b1ef6fe676
igor2000xp <igorring@gmail.com>
2022-04-29 08:05:36 +0300

    init: init Store

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,33 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { PmModule } from '../pm/pm.module';
 import { AuthModule } from '../auth/auth.module';
+import { StoreModule } from '@ngrx/store';
+import { metaReducers, reducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './effects/app.effects';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, SharedModule, PmModule, AuthModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    PmModule,
+    AuthModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+        strictStateSerializability: true,
+        strictActionSerializability: true,
+        strictActionWithinNgZone: true,
+        strictActionTypeUniqueness: true,
+      },
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([AppEffects]),
+  ],
 })
 export class CoreModule {}
