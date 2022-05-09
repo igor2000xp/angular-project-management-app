e95f0bfe21443ff65f567de78ca661b809cf3ce8
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-05-09 17:48:33 +0300

    fix: solve eslint errors

--- src/app/pm/components/task-modal/task-modal.component.ts
+++ src/app/pm/components/task-modal/task-modal.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-task-modal',
   templateUrl: './task-modal.component.html',
-  styleUrls: ['./task-modal.component.scss']
+  styleUrls: ['./task-modal.component.scss'],
 })
 export class TaskModalComponent implements OnInit {
 
