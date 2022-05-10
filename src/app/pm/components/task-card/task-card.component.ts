7d7d1f66b606bb2ecec457c6732cd17b0c837409
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-05-10 22:22:55 +0300

    feat: add create task logic

--- src/app/pm/components/task-card/task-card.component.ts
+++ src/app/pm/components/task-card/task-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Task } from 'src/app/auth/models/Task.model';
 import { TaskCardModalComponent } from '../task-card-modal/task-card-modal.component';
 
 @Component({
@@ -9,9 +10,14 @@ import { TaskCardModalComponent } from '../task-card-modal/task-card-modal.compo
 })
 export class TaskCardComponent implements OnInit {
 
+  @Input() task: Task;
+
+  title: string;
+
   constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
+    this.title = this.task.title;
   }
 
   openTask() {
