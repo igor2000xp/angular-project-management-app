833aadaf0fc1e7e9892c5bcd5b14cb8123736b84
igor2000xp <igorring@gmail.com>
2022-05-13 13:06:02 +0300

    feat: create form

--- src/app/pm/components/tools-menu/tools-menu.component.ts
+++ src/app/pm/components/tools-menu/tools-menu.component.ts
@@ -2,6 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ColumnModalComponent } from '../column-modal/column-modal.component';
 import { TaskModalComponent } from '../task-modal/task-create-modal.component';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+export interface IValidatorMessages {
+  searchString: {
+    required:string;
+    minLength:string;
+    maxLength:string;
+  };
+}
+
 
 @Component({
   selector: 'app-tools-menu',
@@ -10,7 +20,27 @@ import { TaskModalComponent } from '../task-modal/task-create-modal.component';
 })
 export class ToolsMenuComponent implements OnInit {
 
+  public adminForm = new FormGroup({
+    searchString: new FormControl('', [
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.required,
+    ]),
+  });
+
+  public validatorMessages:IValidatorMessages = {
+    searchString:  {
+      required: 'Please enter a search string',
+      minLength: 'The search string is too short',
+      maxLength: 'The search string is too long',
+    },
+  };
+
   constructor(public dialog: MatDialog) {}
+
+  handlerSubmit():void {
+
+  }
 
   openColumn() {
     const dialogRef = this.dialog.open(ColumnModalComponent);
